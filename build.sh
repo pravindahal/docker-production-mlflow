e4c58cf4a66fafbce9e364ebabbb0dc8dddfb4b1
Pravin Dahal <pravin.dahal@optimeering.com>
2021-03-02 11:51:26 +0100

    Double quotes for tag

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-MLFLOW_VERSION=$(cat requirements.txt | grep mlflow | sed -E 's/.*==(.*)$/\1/')
 MLFLOW_VERSION=$(grep mlflow requirements.txt | sed -E 's/.*==(.*)$/\1/')
 
-docker build . -t pravindahal/mlflow:$MLFLOW_VERSION
+docker build . -t "pravindahal/mlflow:$MLFLOW_VERSION"
 
 echo "Built pravindahal/mlflow:$MLFLOW_VERSION, run docker push pravindahal/mlflow:$MLFLOW_VERSION"
